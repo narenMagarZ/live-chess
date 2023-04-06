6ef72f8269b83de7317f94bd48a3d58d2cdf94d2
narenMagarZ <narenmagarz98@gmail.com>
2023-04-06 20:22:44 +0545

    cell block for board

---
+++ frontend/src/components/board/chess-cell.tsx
@@ -0,0 +1,41 @@
+import { Avatar, Paper, Grid } from "@mui/material"
+
+function ChessCell({
+     cellProps,
+     onClick,
+}:TChessCell){
+
+     return(
+          <Paper
+          data-pos={`${cellProps.position.x}-${cellProps.position.y}`}
+          onClick={onClick}
+          sx={{
+               bgcolor:cellProps.bgColor,
+               height:80,
+               width:80,
+               borderRadius:0,
+               cursor:'pointer'
+          }}
+          >
+               <Grid
+               container
+               justifyContent='center'
+               alignItems='center'
+               sx={{
+                    height:'100%'
+               }}
+               >
+                    {
+                         cellProps.player && <Avatar
+                         src={`${cellProps.playerPath}`}
+                         alt=''
+                         />
+                    }
+               
+                    
+               </Grid>
+          </Paper>
+     )
+}
+
+export default ChessCell
