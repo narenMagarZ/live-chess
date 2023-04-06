494870f8271a3cd5ee87e2bea3e25aabd52a0d95
narenMagarZ <narenmagarz98@gmail.com>
2023-04-06 20:21:53 +0545

    utils class, that has methods like, lte and gte

---
+++ frontend/src/utils/index.ts
@@ -0,0 +1,13 @@
+
+
+class Utils {
+     public lte(x:number,X:number){
+          return x <= X
+     }
+     public gte(x:number,X:number){
+          return x >= X
+     }
+}
+
+const u = new Utils()
+export default u
