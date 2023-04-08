0e0f7cf6f7338c3ed7d2558d7fbea618879f1168
narenMagarZ <narenmagarz98@gmail.com>
2023-04-08 20:55:33 +0545

    feat: ask for username first, then add and update the list of players on each connected client

--- frontend/src/components/playground/index.tsx
+++ frontend/src/components/playground/index.tsx
@@ -1,28 +1,77 @@
-import { Container, TextField, Grid, Paper, List } from "@mui/material"
-import { useContext, useEffect, useRef, useState } from "react"
+import { Container, Grid, Paper, Alert, Snackbar } from "@mui/material"
+import { useContext, useEffect, useState } from "react"
 import Board from "../board"
 import Match from "./match"
 import Player from "./player"
 import { socketContext } from "../../app"
 import PlayerLists from "./players-list"
+import Emitter from "../../sockets/emitter"
+import Events from "../../sockets/events"
 function Playground(){
      const socket = useContext(socketContext)
-     const nameFieldRef = useRef<HTMLInputElement>(null)
-     const [players,setPlayers] = useState<IPlayers[]>([])
-     let player:Player
-     socket.on('create-player',(playerId)=>{
-          player = new Player(playerId)
+     const [players,setPlayers] = useState<IPlayer[]>([])
+     const [you,setYou] = useState<IPlayer|null>(null)
+     const [alertMessage,setAlertMessage] = useState('')
+     const [alertBoxState,setAlertBoxState] = useState(false)
+     const [player,setPlayer] = useState<Player|null>(null)
+     const [emitter,setEmitter] = useState<Emitter|null>()
+     const [event,setEvent] = useState<Events|null>()
+     event?.error((data)=>{
+          const {
+               message,
+               type
+          } = data
+          console.log(data,'this is data')
+          setAlertBoxState(true)
+          setAlertMessage(message)
      })
-     socket.on('player-list',(players)=>{
-          console.log(players)
+     event?.playerList((data:{
+          players:IPlayer[]
+     })=>{
+          setPlayers(data.players)
+     })
+     socket.on('createPlayer',(playerInfo)=>{
+          console.log(playerInfo,'playerInfo')
+          const {
+               you,
+               players
+          } = playerInfo
+          const {
+               id,
+               username
+          } = you
+          setPlayer(new Player(id,username))
           setPlayers(players)
+          setYou(you)
      })
-     function setUserName(){
-          socket.emit('set-name',nameFieldRef.current?.value)
+
+     const [username,setUsername] = useState('')
+     function askForUsername(){
+          const promptValue = prompt('Enter username...')
+          if(!promptValue){
+               askForUsername()
+          }
+          else {
+               setUsername(promptValue)
+          }
+     }
+
+     function handleAlertBoxClose(){
+          setAlertBoxState(false)
+          setAlertMessage('')
      }
      useEffect(()=>{
-          console.log(socket)
+          setEmitter(new Emitter(socket))
+          setEvent(new Events(socket))
      },[socket])
+     useEffect(()=>{
+          if(username)
+          emitter?.createPlayer(username)
+     },[username,emitter])
+     useEffect(()=>{
+          if(!username)
+          askForUsername()
+     },[])
      return (
           <Container
           style={{
@@ -30,6 +79,22 @@ function Playground(){
           }}
           maxWidth='lg'
           >
+               <Paper
+               sx={{
+                    boxShadow:'none',
+               }}
+               >
+                    {
+                         alertMessage && <Snackbar open={alertBoxState} autoHideDuration={6000} >
+                         <Alert severity="error" 
+                         onClose={handleAlertBoxClose}
+                         sx={{ width: '100%' }}>
+                           {alertMessage}
+                         </Alert>
+                       </Snackbar>
+                    }
+               </Paper>               
+
                <Grid
                container
                justifyContent='center'
@@ -39,31 +104,17 @@ function Playground(){
                     marginTop:5
                }}
                >
-                    <div
-                    >
-                         <input
-                         ref={nameFieldRef}
-                         style={{
-                              padding:8,
-                              borderRadius:6,
-                              border:'1px solid #000'
-                         }}
-                         placeholder="please set your name to start game..."
-                         />
-                         <button
-                         onClick={()=>setUserName()}
-                         >
-                              Set
-                         </button>
-                    </div>
                </Grid>
                <Grid
                container
+               spacing={2}
                justifyContent='center'
                sx={{
                }}
                >
-                    <PlayerLists players={players} />
+                    <PlayerLists 
+                    you={you}
+                    players={players} />
                     <Board/>
                </Grid>
           </Container>
