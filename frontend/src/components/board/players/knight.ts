65e910304b3a54017aa18a202cd29769bc9ba1da
narenMagarZ <narenmagarz98@gmail.com>
2023-04-12 21:50:54 +0545

    add class knight with method, getPossibleMoves to find the possible movements for knight

---
+++ frontend/src/components/board/players/knight.ts
@@ -0,0 +1,35 @@
+import Piece from "./piece"
+
+class Knight extends Piece{
+
+     getPossibleMoves(currentPos:[number,number]){
+          let[x,y]=currentPos
+          let t = -2
+          for(let i = 0 ; i < 5 ; i++){
+               if(t){
+                    let z = t<0?(t*-1):t
+                    for(let j = 0 ; j < 2;j++){
+                         for(let k = 0 ; k<1;k++){
+                              z = j===0?(z===2)?(z-1):(z+1):(z*-1)
+                              const X = x+t
+                              const Y = y-z
+                              if((X>=this.lowerBound && X<=this.upperBound)&&(Y>=this.lowerBound && Y<=this.upperBound)){
+                                   const piece = this.pieces[X][Y]
+                                   if(piece.player){
+                                        if(piece.type==='remote'){
+                                                  this.possibleMoves.push([X,Y])
+                                        }
+                                   }
+                                   else this.possibleMoves.push([X,Y])
+                              }
+                         }
+                    }
+               }
+               t= t + 1
+          }
+     }
+
+}
+
+
+export default Knight
