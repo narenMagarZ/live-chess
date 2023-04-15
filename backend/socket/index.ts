b599a3c4fee6a1227cab80f6f454979b63e22b42
narenMagarZ <narenmagarz98@gmail.com>
2023-04-15 20:37:42 +0545

    initialize the matchEvent object with player object for both player1 and player2 match events

--- backend/socket/index.ts
+++ backend/socket/index.ts
@@ -17,9 +17,9 @@ class SocketManager {
           event.createPlayer((player,players)=>{
                const playerEvent = new PlayerEvent(player,socket,players,this.io)
                playerEvent.friendlyMatchRequest()
-               playerEvent.acceptMatchRequest(matches,(match,player1Socket)=>{
-                    const player1MatchEvent = new MatchEvent(player1Socket,players,this.io,match)
-                    const player2MatchEvent = new MatchEvent(socket,players,this.io,match)
+               playerEvent.acceptMatchRequest(matches,(match,player1,player2,player1Socket)=>{
+                    const player1MatchEvent = new MatchEvent(player1Socket,player1,players,this.io,match,'player1')
+                    const player2MatchEvent = new MatchEvent(socket,player2,players,this.io,match,'player2')
                     player1MatchEvent.movePiece()
                     player2MatchEvent.movePiece()
                })
