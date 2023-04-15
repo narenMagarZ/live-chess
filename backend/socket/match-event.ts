7e6b0ab7daa6a73794638085600fee2b012d221a
narenMagarZ <narenmagarz98@gmail.com>
2023-04-15 20:36:44 +0545

    emit match end event, when any one king is murdered

--- backend/socket/match-event.ts
+++ backend/socket/match-event.ts
@@ -1,17 +1,20 @@
 import { Socket } from 'socket.io';
-import Player from '../model/player';
 import Match from '../model/match'
-import u from '../utils';
-import Matches from '../model/matches';
 import Events from './events'
 import Players from '../model/player-list';
 import { Server } from 'socket.io';
+import Player from '../model/player';
 
+type TPlayerType = 'player1'|'player2'
 class MatchEvent extends Events{
      match:Match
-     constructor(socket:Socket,players:Players,io:Server,match:Match){
+     player:Player
+     playerType:TPlayerType
+     constructor(socket:Socket,player:Player,players:Players,io:Server,match:Match,playerType:TPlayerType){
           super(socket,players,io)
           this.match = match
+          this.player = player
+          this.playerType = playerType
      }
      public movePiece(){
           this.socket.on('movePiece',(data)=>{
@@ -20,12 +23,26 @@ class MatchEvent extends Events{
                     lastPos,
                     newPos,
                     isPieceMurdered,
-                    isKingMurdered
+                    isKingMurdered,
+                    playerType
                }=data
                const {player1,player2, movementTurn} = this.match
-               
+               const matchId = this.match.getMatchId
                if(isKingMurdered){
                     this.match.isKingMurdered = true
+                    this.socket.to(matchId).emit('matchEnd',{
+                         message:'you loss the match, better luck next time!'
+                    })
+                    this.socket.emit('matchEnd',{
+                         message:'you win the match, congrats!'
+                    })
+                    return
+                    // if(playerType==='player1' && playerType===this.playerType){
+                    //      // here distinguish which one is player1 and player2
+
+                    // }else {
+
+                    // }
                     // now its time to end the game
                     // now remove all the sockets or player connected to this match
                     // emit the matchEnd event
@@ -38,16 +55,17 @@ class MatchEvent extends Events{
                const [nX,nY]=newPos
 
                const newMovementTurn = movementTurn === 'player1' ? 'player2' : 'player1'
+               this.match.movementTurn = newMovementTurn
                // here needs to calculate the new position using pos where pos is in the
                // form of [x,y]
-               const matchId = this.match.getMatchId
                
                // here we are using room to emit the event because
                // it only emit the events for other all connected sockets except itself
 
                this.socket.to(matchId).emit('movePiece',{
-                    lastPos:[Math.abs(7-lX),Math.abs(7-lY)],
-                    newPos:[Math.abs(7-nX),Math.abs(7-nY)],
+                    lastPos:[Math.abs(7-lX),Math.abs(lY)],
+                    newPos:[Math.abs(7-nX),Math.abs(nY)],
+                    movementTurn:newMovementTurn
                })
                
 
