d96734d912ea63d208166c66c69e40f239f259ce
narenMagarZ <narenmagarz98@gmail.com>
2023-04-18 21:02:43 +0545

    add logs for connect,disconnect and error events

--- frontend/src/sockets/index.ts
+++ frontend/src/sockets/index.ts
@@ -5,15 +5,14 @@ const socketUrl = 'http://localhost:5000'
 const socket = io(socketUrl)
 
 socket.on('connect',()=>{
-     console.log(socket.id)
+     console.log('Connected')
 })
 
 socket.on('disconnect',()=>{
-     console.log(socket.id)
+     console.log('Disconncted')
 })
-
 socket.on('connect_error',()=>{
-     console.log('connection error')
+     console.log('Error while connecting')
      socket.connect(); // try to reconnect
 })
 export default socket
