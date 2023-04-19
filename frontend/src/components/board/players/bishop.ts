53b7d320ddf8a94ad766c9c0820000fed7e7422c
narenMagarZ <narenmagarz98@gmail.com>
2023-04-19 21:19:13 +0545

    check for bound values, remove the unnecessary loops

--- frontend/src/components/board/players/bishop.ts
+++ frontend/src/components/board/players/bishop.ts
@@ -1,65 +1,45 @@
-import u from "../../../utils"
-class Bishop{
-     possibleMoves:[number,number][]
-     lowerBound:number
-     upperBound:number
-     constructor(){
-          this.lowerBound = 0
-          this.upperBound = 7
-          this.possibleMoves = []
-     }
+import Piece from "./piece"
+
+class Bishop extends Piece{
      public getPossibleMoves(currentPos:[number,number]){
           const [x,y] = currentPos
-          let i=x
-          let j=y
+          let t = -1
           for(let i=0;i<2;i++){
-               let t = -1
                let a = x
                let b = y
                let c = y
+               let forb = false
+               let forc = false
                for(;;){
                     a = a+(1*t)
                     b=b-1
                     c=c+1
-                    
-                    this.possibleMoves.push([a,b])
-                    this.possibleMoves.push([a,c])
+                    if(((a>this.upperBound||a<this.lowerBound)||(b>this.upperBound||b<this.lowerBound)) && 
+                    ((a>this.upperBound||a<this.lowerBound)||(c>this.upperBound||c<this.lowerBound)))
+                    break
+                    if(!((a>this.upperBound||a<this.lowerBound)||(b>this.upperBound||b<this.lowerBound))){
+                         const piece = this.pieces[a][b]
+                         if(piece.player&&!forb){
+                              if(piece.type==='remote'){
+                                   this.possibleMoves.push([a,b])
+                              }
+                              forb = true
+                         }else if(!forb) 
+                         this.possibleMoves.push([a,b])
+                    }
+                    if(!((a>this.upperBound||a<this.lowerBound)||(c>this.upperBound||c<this.lowerBound))){
+                         const piece = this.pieces[a][c]
+                         if(piece.player&&!forc){
+                              if(piece.type==='remote'){
+                                   this.possibleMoves.push([a,c])
+                              }
+                              forc = true
+                         }
+                         else if(!forc)
+                         this.possibleMoves.push([a,c])
+                    }
                }
                t = 1
-          }
-          for(;;){
-               i=i-1
-               j=j-1
-               if(i<this.lowerBound || j<this.lowerBound)
-                    break
-               this.possibleMoves.push([i,j])
-          }
-          i=x 
-          j=y
-          for(;;){
-               i=i+1
-               j=j+1
-               if(i>this.upperBound||j>this.upperBound)
-               break
-               this.possibleMoves.push([i,j])
-          }
-          i=x
-          j=y
-          for(;;){
-               i=i-1
-               j=j+1
-               if(i<this.lowerBound||j>this.upperBound)
-               break
-               this.possibleMoves.push([i,j])
-          }
-          i=x
-          j=y
-          for(;;){
-               i=i+1
-               j=j-1
-               if(i>this.upperBound||j<this.lowerBound)
-               break
-               this.possibleMoves.push([i,j])
           }
      }
 }
