e9d9358c2ef004f054a22420ef53223e8b906821
narenMagarZ <narenmagarz98@gmail.com>
2023-04-19 21:18:04 +0545

    define getPossibleMoves function

--- frontend/src/components/board/players/pawn.ts
+++ frontend/src/components/board/players/pawn.ts
@@ -1,32 +1,22 @@
 import Piece from "./piece"
 
 class Pawn extends Piece{
-     public findCell(){
-          const x = this.position.x
-          const y = this.position.y
-          const pos = `${x.toString()}-${y.toString()}`
-          this.cell = document.querySelector(`[data-pos="${pos}"]`)
-     }
-     public findPossiblePath(): void {
-          if(this.cell){
-               const [i,j] = this.cell.dataset.pos?.split('-') || []
-               if(i && j){
-                    const posX = parseInt(i)
-                    const posY = parseInt(j)
-                    const paths = [[posX-1,posY],[posX-1,posY-1],[posX-1,posY+1]]
-                    for(let i of paths){
-                         console.log(i)
-                         const [lowerBound,uppperBound] = i
-                         if(lowerBound !>= this.lowerBound && 
-                              uppperBound !<= this.uppperBound){
-                                   super.findCell(lowerBound,uppperBound)
-                         }
-                    }
+     public getPossibleMoves(currentPos: TCurrentPos): void {
+          const[x,y]=currentPos
+          let piece = this.pieces[x-1][y]
+          if((x-1) >= this.lowerBound && !piece.player){
+               this.possibleMoves.push([x-1,y])
+          }    
+          piece = this.pieces[x-1][y-1]
+          if((x-1)>=this.lowerBound && (y-1)>=this.lowerBound && piece.player && piece.type==='remote')
+               this.possibleMoves.push([x-1,y-1])
 
-               }
+          piece = this.pieces[x-1][y+1]
+          if((x-1)>=this.lowerBound && (y+1)<=this.upperBound && piece.player && piece.type==='remote')
+               this.possibleMoves.push([x-1,y+1])
 
-          }
      }
 }
+
 
 export default Pawn
